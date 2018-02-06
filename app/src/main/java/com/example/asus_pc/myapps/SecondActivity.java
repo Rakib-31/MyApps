be59163d1222a568cf41da21441560436c177360
Rakib31 <30778508+Rakib31@users.noreply.github.com>
2018-02-07 02:43:35 +0600

    Initial commit

---
+++ app/src/main/java/com/example/asus_pc/myapps/SecondActivity.java
@@ -0,0 +1,83 @@
+package com.example.asus_pc.myapps;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button b;
+    private EditText name,mail,phone,age,passward;
+    LoginClass myDatabaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        b = (Button) findViewById(R.id.buttonIsClicked);
+        name = (EditText) findViewById(R.id.nameBox);
+        mail = (EditText) findViewById(R.id.mailBox);
+        phone = (EditText) findViewById(R.id.phoneBox);
+        age = (EditText) findViewById(R.id.ageBox);
+        passward = (EditText) findViewById(R.id.passwardBox);
+
+        myDatabaseHelper = new LoginClass(this);
+        SQLiteDatabase sqLiteDatabase = myDatabaseHelper.getWritableDatabase();
+
+        b.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick (View v){
+        if(!validity())
+        {
+            String Name = name.getText().toString();
+            String Mail = mail.getText().toString();
+            String Age = age.getText().toString();
+            String Passward = passward.getText().toString();
+
+            myDatabaseHelper.insertData(Name,Mail,Passward,Age);
+
+            Intent inten = new Intent(SecondActivity.this, ThirdActivity.class);
+            startActivity(inten);
+        }
+    }
+
+    private boolean validity()
+    {
+        if(name.getText().toString().trim().length() <= 0)
+        {
+            Toast.makeText(SecondActivity.this,"Enter User Name",Toast.LENGTH_LONG).show();
+            return true;
+        }
+        else if(mail.getText().toString().trim().length() <= 0)
+        {
+            Toast.makeText(SecondActivity.this,"Enter E-mail",Toast.LENGTH_LONG).show();
+            return true;
+        }
+        else if(phone.getText().toString().trim().length() <= 0)
+        {
+            Toast.makeText(SecondActivity.this,"Enter Phone Number",Toast.LENGTH_LONG).show();
+            return true;
+        }
+        else if(age.getText().toString().trim().length() <= 0)
+        {
+            Toast.makeText(SecondActivity.this,"Enter Age",Toast.LENGTH_LONG).show();
+            return true;
+        }
+        else if(passward.getText().toString().trim().length() <= 0)
+        {
+            Toast.makeText(SecondActivity.this,"Enter Passward",Toast.LENGTH_LONG).show();
+            return true;
+        }
+        else
+            return false;
+    }
+}
---
+++ app/src/main/java/com/example/asus_pc/myapps/ThirdActivity.java
@@ -0,0 +1,89 @@
+package com.example.asus_pc.myapps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ThirdActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText pass,email,url;
+    private Button button,button2;
+    public  String string = new String();
+
+    LoginClass loginHelper = new LoginClass(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        email = (EditText) findViewById(R.id.email);
+        pass = (EditText) findViewById(R.id.pass);
+        url = (EditText) findViewById(R.id.URL);
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(this);
+        button2.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.button) {
+
+            if (!validity()) {
+                Log.d("tag",string);
+
+                if (loginHelper.search(email.getText().toString(), pass.getText().toString())) {
+                    String string = url.getText().toString().trim();
+                    url.setText("");
+                    email.setText("");
+                    pass.setText("");
+                    Intent inten = new Intent(ThirdActivity.this, FourthActivity.class);
+                    inten.putExtra("NAME",string);
+                    startActivity(inten);
+                } else
+                    Toast.makeText(ThirdActivity.this, "incorrect username or passward", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        else if(v.getId() == R.id.button2)
+        {
+            Intent inten = new Intent(ThirdActivity.this, SecondActivity.class);
+            startActivity(inten);
+        }
+    }
+
+    private boolean validity()
+    {
+        if(url.getText().toString().trim().length() <= 0)
+        {
+            Toast.makeText(ThirdActivity.this,"Enter url",Toast.LENGTH_LONG).show();
+            return true;
+        }
+        else if(email.getText().toString().trim().length() <= 0)
+        {
+            Toast.makeText(ThirdActivity.this,"Enter username",Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        else if(pass.getText().toString().trim().length() <= 0)
+        {
+            Toast.makeText(ThirdActivity.this,"Enter passward",Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        else
+            return false;
+    }
+
+}
